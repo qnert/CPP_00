399c905b4f1a3799c3ecc7e7614b6daf334b20c9
qnert <127817637+qnert@users.noreply.github.com>
2023-09-04 19:00:04 +0200

    Makefile change and some Zombie changes

--- CPP_01/ex00/newZombie.cpp
+++ CPP_01/ex00/newZombie.cpp
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 18:13:06 by skunert           #+#    #+#             */
-/*   Updated: 2023/09/04 18:43:39 by skunert          ###   ########.fr       */
+/*   Updated: 2023/09/04 18:51:04 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 Zombie* newZombie(std::string name)
 {
-	Zombie *Z = new Zombie(name);
-	return (Z);
+	Zombie *zom = new Zombie(name);
+	return (zom);
 }
