3e06a85607cdad45228a0b3151fd82017636a5d2
qnert <127817637+qnert@users.noreply.github.com>
2023-10-01 13:46:17 +0200

    code is finished just need to test everything in detail

---
+++ CPP_01/ex06/Harl.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 14:42:10 by skunert           #+#    #+#             */
+/*   Updated: 2023/10/01 13:45:36 by skunert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+}
+
+Harl::~Harl(void)
+{
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\n" << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n" << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	Harl person;
+	int	i;
+	void	(Harl::*funcptrs[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string	func_names[4] = {"debug", "info", "warning", "error"};
+
+	i = 0;
+	while (i < 4)
+	{
+		if (!level.compare(func_names[i]))
+			break ;
+		i++;
+	}
+	switch(i)
+	{
+		case 0:
+			(person.*funcptrs[0])();
+		case 1:
+			(person.*funcptrs[1])();
+		case 2:
+			(person.*funcptrs[2])();
+		case 3:
+			(person.*funcptrs[3])();
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]\n";
+	}
+	return ;
+}
