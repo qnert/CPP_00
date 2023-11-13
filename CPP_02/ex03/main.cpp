c8f9ab32459899f620bf956d4ce0c17f5b1e61d9
Simon Kunert <skunert@1-A-4.42heilbronn.de>
2023-11-13 19:48:26 +0100

    minor changes

--- CPP_02/ex03/main.cpp
+++ CPP_02/ex03/main.cpp
@@ -6,11 +6,13 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 14:33:33 by skunert           #+#    #+#             */
-/*   Updated: 2023/11/13 19:40:09 by skunert          ###   ########.fr       */
+/*   Updated: 2023/11/13 19:47:26 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
+
+//https://planetcalc.com/8108/#google_vignette
 
 int main(void) {
 	// Triangle
@@ -18,17 +20,14 @@ int main(void) {
 	Point b = Point(4, 5);
 	Point c = Point(8, -2);
 
-	//first points
 	Point p1 = Point(5, 4.67f);
 
-	//second points
 	Point p2 = Point(9.5f, 2.5f);
 
-	//third points
 	Point p3 = Point(5.42f, 2.53f);
 
-	std::cout << "p1: " << std::boolalpha << bsp(a, b, c, p1) << std::endl;
-	std::cout << "p2: " << std::boolalpha << bsp(a, b, c, p2) << std::endl;
-	std::cout << "p3: " << std::boolalpha << bsp(a, b, c, p3) << std::endl;
+	std::cout << "Is point 1 on the triangle? " << std::boolalpha << bsp(a, b, c, p1) << std::endl;
+	std::cout << "Is point 2 on the triangle? " << std::boolalpha << bsp(a, b, c, p2) << std::endl;
+	std::cout << "Is point 3 on the triangle? " << std::boolalpha << bsp(a, b, c, p3) << std::endl;
 	return (0);
 }
