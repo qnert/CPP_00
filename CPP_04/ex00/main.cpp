63c4132cfcc067e8eed9ca736ac411a01f6a475b
qnert <127817637+qnert@users.noreply.github.com>
2023-11-16 15:06:00 +0100

    some own tests and leaks checked

--- CPP_04/ex00/main.cpp
+++ CPP_04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 12:25:20 by skunert           #+#    #+#             */
-/*   Updated: 2023/11/16 12:43:41 by skunert          ###   ########.fr       */
+/*   Updated: 2023/11/16 15:05:33 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,44 @@
 #include "WrongCat.hpp"
 
 int main() {
-  const Animal* meta = new Animal();
-  const Animal* j = new Dog();
-  const Animal* i = new Cat();
-  std::cout << j->getType() << " " << std::endl;
-  std::cout << i->getType() << " " << std::endl;
-  i->makeSound();  // will output the cat sound!
-  j->makeSound();
-  meta->makeSound();
+	const Animal* meta = new Animal();
+	const Animal* D1 = new Dog();
+	const Animal* C1 = new Cat();
+	std::cout << D1->getType() << " " << std::endl;
+	std::cout << C1->getType() << " " << std::endl;
+	C1->makeSound();  // will output the cat sound!
+	D1->makeSound();
+	meta->makeSound();
 
-  delete meta;
-  delete j;
-  delete i;
+	delete meta;
+	delete D1;
+	delete C1;
 
-  const WrongAnimal* meta2 = new WrongAnimal();
-  const Animal* j2 = new Dog();
-  const WrongAnimal* i2 = new WrongCat();
-  std::cout << j2->getType() << " " << std::endl;
-  std::cout << i2->getType() << " " << std::endl;
-  i2->makeSound();  // will output the wrong animal sound!
-  j2->makeSound();
-  meta2->makeSound();
+	std::cout << std::endl;
 
-  delete meta2;
-  delete j2;
-  delete i2;
-  std::cout << "my own tests start here" << std::endl;
-  Animal animal;
-  WrongCat wrongcat;
-  Cat cat;
+	const WrongAnimal* meta2 = new WrongAnimal();
+	const Animal* D2 = new Dog();
+	const WrongAnimal* C2 = new WrongCat();
+	std::cout << D2->getType() << " " << std::endl;
+	std::cout << C2->getType() << " " << std::endl;
+	C2->makeSound();	// should output the wronganimal sound to show the effect of virtual funcs
+	D2->makeSound();
+	meta2->makeSound();
 
-  animal.makeSound();
-  wrongcat.makeSound();
-  cat.makeSound();
-  // system("leaks a.out");
+	delete meta2;
+	delete D2;
+	delete C2;
+
+	std::cout << std::endl;
+
+	Animal	meta3;
+	Dog		D3;
+	Cat		C3;
+
+	D3.makeSound();
+	C3.makeSound();
+	std::cout << meta3.getType() << std::endl;
+	meta3.makeSound();
+	// system("leaks pets");
   return 0;
 }
