df3842f34a314fb97dcd431cf627171010c49ad8
qnert <127817637+qnert@users.noreply.github.com>
2023-11-21 18:45:00 +0100

    some easier test for demonstration

--- CPP_03/ex03/main.cpp
+++ CPP_03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 11:00:45 by skunert           #+#    #+#             */
-/*   Updated: 2023/11/21 18:38:13 by skunert          ###   ########.fr       */
+/*   Updated: 2023/11/21 18:43:59 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@ int main(void)
 {
 	DiamondTrap DT1("Walter");
 
+	std::cout << std::endl;
 	DT1.whoAmI();
-	DT1.FragTrap::attack("his evaluator");
 	DT1.attack("his evaluator");
 	DT1.guardGate();
 	DT1.beRepaired(1);
-	for (int i = 0; i < 47; i++)
+	for (int i = 0; i < 4; i++)
 		DT1.attack("a stone");
 	DT1.beRepaired(50);
 	DT1.whoAmI();
+	std::cout << std::endl;
 }
