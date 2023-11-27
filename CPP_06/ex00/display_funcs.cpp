29d592fe74649f4a1a2169ec413b6c1c53a53c25
Simon Kunert <skunert@1-A-8.42heilbronn.de>
2023-11-27 15:46:51 +0100

    some more checks added

--- CPP_06/ex00/display_funcs.cpp
+++ CPP_06/ex00/display_funcs.cpp
@@ -6,19 +6,21 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 13:21:22 by skunert           #+#    #+#             */
-/*   Updated: 2023/11/27 15:25:47 by skunert          ###   ########.fr       */
+/*   Updated: 2023/11/27 15:46:13 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <iomanip>
+#include <cstring>
 
 void	display_char(std::string str)
 {
-	int		i = std::atoi(str.c_str());
-	char	c = std::atoi(str.c_str());
+	int		i			= std::atoi(str.c_str());
+	char	c			= std::atoi(str.c_str());
+	std::string check	= "infinffnannanf-inf-inff";
 
-	if (i > 127)
+	if (i > 127 || check.find(str))
 		std::cout << "char : impossible" << std::endl;
 	else if (i < 32 || i == 127)
 		std::cout << "char : Non displayable" << std::endl;
@@ -28,9 +30,11 @@ void	display_char(std::string str)
 
 void	display_int(std::string str)
 {
-	int	nb = std::atoi(str.c_str());
+	int			nb		= std::atoi(str.c_str());
+	std::string check	= "infinffnannanf-inf-inff";
 
-	if (std::atoll(str.c_str()) > INT_MAX || std::atoll(str.c_str()) < INT_MIN){
+	if (std::atoll(str.c_str()) > INT_MAX || std::atoll(str.c_str()) < INT_MIN
+		|| check.find(str)){
 		std::cout << "int : impossible" << std::endl;
 		return ;
 	}
@@ -40,7 +44,7 @@ void	display_int(std::string str)
 void	display_float(std::string str)
 {
 	float	ft = std::atof(str.c_str());
-	if (str.find('.') == 0)
+	if (!std::strchr(str.c_str(), '.'))
 		std::cout << std::fixed << std::setprecision(1) << "float : " << ft << "f" << std::endl;
 	else
 		std::cout << std::fixed << "float : " << ft << "f" << std::endl;
@@ -50,7 +54,7 @@ void	display_double(std::string str)
 {
 	float	db = std::atof(str.c_str());
 
-	if (str.find('.') == 0)
+	if (!std::strchr(str.c_str(), '.'))
 		std::cout << std::fixed << std::setprecision(1) << "double : " << db << std::endl;
 	else
 		std::cout << std::fixed << "double : " << db << std::endl;
