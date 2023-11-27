d4072df265ecc8aaada08464dc184c640d312543
Simon Kunert <skunert@1-A-8.42heilbronn.de>
2023-11-27 18:04:46 +0100

    little fix but needs more tests

--- CPP_06/ex00/ScalarConverter.cpp
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 10:43:19 by skunert           #+#    #+#             */
-/*   Updated: 2023/11/27 17:29:11 by skunert          ###   ########.fr       */
+/*   Updated: 2023/11/27 18:04:27 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,18 @@ void	ScalarConverter::convert(const std::string str)
 		return ;
 	}
 	if (is_char(str)){
-		std::cout << "char" << std::endl;
 		conversion_char(str[0]);
 		return ;
 	}
 	if (is_int(str)){
-		std::cout << "int" << std::endl;
 		conversion_int(std::atoi(str.c_str()));
 		return ;
 	}
 	if (is_double(str)){
-		std::cout << "double" << std::endl;
 		conversion_double(std::strtod(str.c_str(), NULL));
 		return ;
 	}
 	if (is_float(str)){
-		std::cout << "float" << std::endl;
 		conversion_float(std::atof(str.c_str()));
 		return ;
 	}
