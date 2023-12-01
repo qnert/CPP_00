0e3c3c1150eec4f17f997d51b24588ddbc2ec847
qnert <127817637+qnert@users.noreply.github.com>
2023-12-01 14:47:13 +0100

    eval test added

--- CPP_07/ex00/main.cpp
+++ CPP_07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 16:18:29 by skunert           #+#    #+#             */
-/*   Updated: 2023/11/28 16:29:38 by skunert          ###   ########.fr       */
+/*   Updated: 2023/12/01 14:46:58 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,36 @@ int main(void){
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return 0;
 }
+
+
+// class Awesome{
+// 	public:
+// 		Awesome(void): _n(0){}
+// 		Awesome (int n): _n(n){}
+// 		Awesome&	operator=(Awesome& a){_n = a._n; return (*this);}
+// 		bool		operator==(Awesome const &rhs) const {return (this->_n == rhs._n);}
+// 		bool		operator!=(Awesome const &rhs) const {return (this->_n != rhs._n);}
+// 		bool		operator>(Awesome const &rhs) const {return (this->_n > rhs._n);}
+// 		bool		operator<(Awesome const &rhs) const {return (this->_n < rhs._n);}
+// 		bool		operator>=(Awesome const &rhs) const {return (this->_n >= rhs._n);}
+// 		bool		operator<=(Awesome const &rhs) const {return (this->_n <= rhs._n);}
+// 		int			get_n(void) const {return (_n);}
+
+// 	private:
+// 		int			_n;
+// };
+
+// std::ostream&	operator<<(std::ostream& os, Awesome const& other){
+// 	os << other.get_n(); return (os);
+// }
+
+// int main(void)
+// {
+// 	Awesome a(2), b(4);
+
+// 	swap(a, b);
+// 	std::cout << a << " " << b << std::endl;
+// 	std::cout << max(a, b) << std::endl;
+// 	std::cout << min(a, b) << std::endl;
+// 	return (0);
+// }
